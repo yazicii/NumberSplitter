c566b597ce6ecc9ea395bd822ebee6d45de27834
oguzhan.yazici <oguzhan.yazici@enuygun.com>
2023-07-10 02:19:19 +0300

    first commit

--- NumberSplitterFirst/src/main/java/Main.java
+++ NumberSplitterFirst/src/main/java/Main.java
@@ -1,7 +1,49 @@
-package org.example;
+import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main extends NumberSplitter{
+
+
+    public Main(int startIndex, int endIndex) {
+        super(startIndex, endIndex);
     }
+
+    public static void main(String[] args) {
+
+        numbers = new ArrayList<>();
+        evenNumbers = new ArrayList<>();
+        oddNumbers = new ArrayList<>();
+
+        for (int i = 1; i <= 100; i++) {
+            numbers.add(i);
+        }
+
+        int chunkSize = numbers.size() / 4;
+        int starIndex = 0;
+        int endIndex = chunkSize;
+
+        for (int i = 0; i < 4; i++) {
+            NumberSplitter numberSplitter = new NumberSplitter(starIndex, endIndex);
+            new Thread(numberSplitter).start();
+            starIndex = endIndex;
+            endIndex += chunkSize;
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Even Numbers");
+        for (int number : evenNumbers) {
+            System.out.println(number);
+        }
+
+        System.out.println("Odd Numbers");
+        for (int number : oddNumbers) {
+            System.out.println(number);
+        }
+
+    }
+
 }
--- NumberSplitterFirst/src/main/java/NumberSplitter.java
+++ NumberSplitterFirst/src/main/java/NumberSplitter.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class NumberSplitter {
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class NumberSplitter implements Runnable {
+
+    protected static List<Integer> numbers;
+    protected static List<Integer> evenNumbers;
+    protected static List<Integer> oddNumbers;
+    private static final Object LOCK = new Object();
+    private int startIndex;
+    private int endIndex;
+
+    public NumberSplitter(int startIndex, int endIndex) {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+    }
+
+
+    @Override
+    public void run() {
+        for (int i = startIndex; i<endIndex;i++){
+            int number = numbers.get(i);
+            if (number % 2 ==0){
+                synchronized (LOCK) {
+                    oddNumbers.add(i);
+                }
+            }else {
+                synchronized (LOCK) {
+                    evenNumbers.add(i);
+                }
+            }
+        }
+
+    }
 }
